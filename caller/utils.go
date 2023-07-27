4adeec3e7dbd1418b7fa4c99eb8fd9bb9a0014a7
Nelson Lai <nelsonlai9503@gmail.com>
2023-07-27 16:09:58 +0800

    add caller

---
+++ caller/utils.go
@@ -0,0 +1,159 @@
+package caller
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/metadiv-io/ginger"
+	"github.com/metadiv-io/ginger/types"
+)
+
+func get[T any](ctx *gin.Context, host string, path string,
+	params map[string]string, headers map[string]string) (*Response[T], error) {
+	if headers == nil {
+		headers = make(map[string]string)
+	}
+	headers[ginger.HEADER_TRACE_ID] = getTraceID(ctx)
+	headers[ginger.HEADER_TRACE] = getTraces(ctx)
+	headers[ginger.HEADER_LOCALE] = getLocale(ctx)
+	headers["Authorization"] = getAuthToken(ctx)
+
+	path += "?"
+	for k, v := range params {
+		path += k + "=" + v + "&"
+	}
+	path = path[:len(path)-1]
+
+	req, err := http.NewRequest("GET", host+path, nil)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response Response[T]
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		fmt.Println("failed to unmarshal response body: ", string(bodyBytes))
+		return nil, err
+	}
+
+	if ctx != nil {
+		setTraceID(ctx, response.TraceID)
+		setTraces(ctx, response.Traces)
+	}
+	return &response, nil
+}
+
+func nonGet[T any](ctx *gin.Context, host, path, method string,
+	body interface{}, headers map[string]string) (*Response[T], error) {
+	if headers == nil {
+		headers = make(map[string]string)
+	}
+	headers[ginger.HEADER_TRACE_ID] = getTraceID(ctx)
+	headers[ginger.HEADER_TRACE] = getTraces(ctx)
+	headers[ginger.HEADER_LOCALE] = getLocale(ctx)
+	headers["Authorization"] = getAuthToken(ctx)
+
+	b, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, host+path, bytes.NewReader(b))
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response Response[T]
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		fmt.Println("failed to unmarshal response body: ", string(bodyBytes))
+		return nil, err
+	}
+
+	if ctx != nil {
+		setTraceID(ctx, response.TraceID)
+		setTraces(ctx, response.Traces)
+	}
+	return &response, nil
+}
+
+func getTraceID(ctx *gin.Context) string {
+	if ctx == nil {
+		return ""
+	}
+	return ctx.GetHeader(ginger.HEADER_TRACE_ID)
+}
+
+func setTraceID(ctx *gin.Context, traceID string) {
+	if ctx == nil {
+		return
+	}
+	ctx.Request.Header.Set(ginger.HEADER_TRACE_ID, traceID)
+}
+
+func getTraces(ctx *gin.Context) string {
+	if ctx == nil {
+		return ""
+	}
+	return ctx.GetHeader(ginger.HEADER_TRACE)
+}
+
+func setTraces(ctx *gin.Context, traces []types.Trace) {
+	if ctx == nil {
+		return
+	}
+	bytes, _ := json.Marshal(traces)
+	ctx.Request.Header.Set(ginger.HEADER_TRACE, string(bytes))
+}
+
+func getLocale(ctx *gin.Context) string {
+	if ctx == nil {
+		return ginger.LOCALE_EN
+	}
+	return ctx.GetHeader(ginger.HEADER_LOCALE)
+}
+
+func getAuthToken(ctx *gin.Context) string {
+	if ctx == nil {
+		return ""
+	}
+	token := ctx.GetHeader("Authorization")
+	token = strings.ReplaceAll(token, "Bearer ", "")
+	token = strings.ReplaceAll(token, "bearer ", "")
+	token = strings.ReplaceAll(token, "BEARER ", "")
+	token = strings.ReplaceAll(token, " ", "")
+	return token
+}
