8ec380435b649daa5d90f0b3d63a46040c839e6c
Nelson Lai <nelsonlai9503@gmail.com>
2023-11-13 17:55:55 +0800

    refactor err map

--- error-map.go
+++ error-map.go
@@ -8,11 +8,14 @@ var errMapObj = errMap{}
 
 type errMap map[string]map[string]string
 
-func RegisterError(code, locale, message string) {
-	if _, ok := errMapObj[locale]; !ok {
-		errMapObj[locale] = make(map[string]string)
+func RegisterError(code, message string, locale ...string) {
+	if len(locale) == 0 {
+		locale = append(locale, locale_default)
 	}
-	errMapObj[locale][code] = message
+	if _, ok := errMapObj[locale[1]]; !ok {
+		errMapObj[locale[1]] = make(map[string]string)
+	}
+	errMapObj[locale[1]][code] = message
 }
 
 func GetError(code string, locale ...string) string {
